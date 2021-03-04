336bd670fefa980a0bb4f34deb26bdbcf3cdc278
Bitrey <info@bitrey.it>
2021-03-04 18:06:21 +0100

    npm start working

--- src/index.ts
+++ src/index.ts
@@ -174,7 +174,7 @@ app.use(bodyParser.json());
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "index.html"));
+    res.sendFile(path.join(__dirname, "..", "index.html"));
 });
 
 app.get("/valid-ip", (req, res) => {
